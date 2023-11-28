e996c39fd7026bb149ff4bf409f74e851795abfc
almlmla <58733471+almlmla@users.noreply.github.com>
2023-11-28 14:48:26 +0800

    modified to always include first second of video.  cleaned up code.

--- ss-video-silence-remover.sh
+++ ss-video-silence-remover.sh
@@ -3,6 +3,20 @@
 # Create a directory for trimmed files
 mkdir -p "./trimmed"
 
+# Function to check and perform ffmpeg cut
+perform_cut() {
+    local start="$1"
+    local end="$2"
+    local file="$3"
+    local num_cuts="$4"
+
+    if (( $(echo "$end > $start" | bc -l) )); then
+        local cut_file="cut${num_cuts}.mp4"
+        ffmpeg -hide_banner -loglevel error -y -ss "$start" -to "$end" -i "$file" -c copy -avoid_negative_ts make_zero "${cut_file}"
+        cut_files+=("$cut_file")
+    fi
+}
+
 # Iterate through all .mp4 files in the current directory
 for file in *.mp4; do
     if [ -f "$file" ]; then
@@ -11,46 +25,53 @@ for file in *.mp4; do
         # Get silence intervals using silencedetect
         silence_info=$(ffmpeg -hide_banner -i "$file" -af "silencedetect=n=-50dB:d=3" -f null - 2>&1)
 
-        selectionsList=()
-        timeSelection="between(t,0,"
+        num_cuts=0
+        cut_files=()
         end=""
-        start=""
+        find_start=""
+        start=0
+
+        # Perform initial cut for the first second
+        perform_cut "0" "1" "$file" "$num_cuts"
+        num_cuts=$((num_cuts+1))
 
         # Read silence information from the provided variable
         while IFS= read -r line; do
             # Detect a start of silence, which is an end of our selection
             end=$(echo "$line" | grep -o "silence_start: [0-9]\+\(\.[0-9]\+\)\?" | cut -d' ' -f2)
             # Detect an end of silence, which is a start of our selection
-            start=$(echo "$line" | grep -o "silence_end: [0-9]\+\(\.[0-9]\+\)\?" | cut -d' ' -f2)
+            find_start=$(echo "$line" | grep -o "silence_end: [0-9]\+\(\.[0-9]\+\)\?" | cut -d' ' -f2)
 
-            if [ -n "$start" ]; then
-                timeSelection="between(t,$start,"
+            if [ -n "$find_start" ]; then
+                start=$find_start
             fi
 
             if [ -n "$end" ]; then
-                timeSelection+="$end)"
-                selectionsList+=("$timeSelection")
+                perform_cut "$start" "$end" "$file" "$num_cuts"
+                num_cuts=$((num_cuts+1))
             fi
 
         done <<< "$silence_info"
 
-        # Note: silencedetect apparently handles properly files that start and/or end in silence
-        # so we don't need to check for that and complete filters with no start or no end
-        selectionFilter="'$(IFS=+; echo "${selectionsList[*]}")'"
-
         echo "Saving output file for $file..."
 
         # Extract filename without extension
-        full_filename=$(basename "$file" | cut -d. -f1)
-
-        # Extract file extension
-        extension=$(basename "$file" | awk -F. '{if (NF>1) {print $NF}}')
+        full_filename=$(basename -- "$file" | cut -d. -f1)
 
         # Create trimmed filename with extension
-        outfile="${full_filename}_trimmed.$extension"
+        outfile="${full_filename}_trimmed.${file##*.}"
 
         # Output ffmpeg command, automatic overwrite
-        ffmpeg -hide_banner -loglevel error -y -i "$file" -vf "select=$selectionFilter,setpts=N/FRAME_RATE/TB" -af "aselect=$selectionFilter,asetpts=N/SR/TB" -c:v libx264 -c:a aac -strict experimental "trimmed/$outfile"
+        ffmpeg -hide_banner -loglevel error -y -f concat -safe 0 -i <(for f in "${cut_files[@]}"; do echo "file '$PWD/$f'"; done) -c copy -avoid_negative_ts 1 "trimmed/$outfile"
+
+        # Handle errors in ffmpeg commands
+        if [ $? -ne 0 ]; then
+            echo "Error: Failed to create trimmed file for $file"
+        else
+            echo "Trimmed file saved: $outfile"
+            # Clean up temporary cut files
+            rm "${cut_files[@]}"
+        fi
     fi
 done
 
